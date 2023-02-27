7b69d55a0f708af3cbe2223951bd52acc0dadfe2
unknown <chahine.benlahcen@gmail.com>
2023-02-27 11:46:57 +0100

    fix: AppDataSource and function typeOrm

--- config/connexion.ts
+++ config/connexion.ts
@@ -1,6 +1,30 @@
 import { DataSource } from "typeorm"
 
-export const myDataSource = new DataSource({})
-export const Manager = myDataSource.manager
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT ?? "5432"),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: [__dirname + '/../models/*.ts'],
+    synchronize: true,
+  })
+  const myDataSource = new DataSource({
+    type: "mongodb",
+    host: "localhost",
+    port: 27017,
+    database: "test",
+  })
+  
+  AppDataSource.initialize()
+      .then(() => {
+          console.log("Data Source has been initialized!")
+      })
+      .catch((err) => {
+          console.error("Error during Data Source initialization", err)
+      })
+  
+export const Manager = AppDataSource.manager
 // export const UserRepository = myDataSource.getRepository(UserEntity)
 // export const PhotoRepository = myDataSource.getRepository(PhotoEntity)
--- config/database.ts
+++ config/database.ts
@@ -1,28 +1,14 @@
-import { DataSource } from "typeorm"
+import { ConnectionOptions } from 'typeorm';
 
-const AppDataSource = new DataSource({
-  type: "postgres",
+const config: ConnectionOptions = {
+  type: 'postgres',
   host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT ?? "5432"),
+  port: parseInt(process.env.DB_PORT),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   entities: [__dirname + '/../models/*.ts'],
   synchronize: true,
-})
-const myDataSource = new DataSource({
-  type: "mongodb",
-  host: "localhost",
-  port: 27017,
-  database: "test",
-})
+};
 
-AppDataSource.initialize()
-    .then(() => {
-        console.log("Data Source has been initialized!")
-    })
-    .catch((err) => {
-        console.error("Error during Data Source initialization", err)
-    })
-
-export default AppDataSource;
+export default config;
