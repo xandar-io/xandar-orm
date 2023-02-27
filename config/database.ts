0c58838d669a7dc8fd0f70012de0d68d6071b74b
unknown <chahine.benlahcen@gmail.com>
2023-02-27 11:39:41 +0100

    fix: AppDataSource and function typeOrm

--- config/database.ts
+++ config/database.ts
@@ -1,14 +1,28 @@
-import { ConnectionOptions } from 'typeorm';
+import { DataSource } from "typeorm"
 
-const config: ConnectionOptions = {
-  type: 'postgres',
+const AppDataSource = new DataSource({
+  type: "postgres",
   host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT),
+  port: parseInt(process.env.DB_PORT ?? "5432"),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   entities: [__dirname + '/../models/*.ts'],
   synchronize: true,
-};
+})
+const myDataSource = new DataSource({
+  type: "mongodb",
+  host: "localhost",
+  port: 27017,
+  database: "test",
+})
 
-export default config;
+AppDataSource.initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!")
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization", err)
+    })
+
+export default AppDataSource;
